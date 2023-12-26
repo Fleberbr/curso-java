1178bee620154b22086135206e827bc6cf27c03a
Fleberbr <eltondelira@gmail.com>
2023-12-26 20:57:47 -0300

    exercicio de matriz

--- exercicios-sessao-10/src/main/java/Funcionario.java
+++ exercicios-sessao-10/src/main/java/Funcionario.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Funcionario {
+public class Funcionario {
+    private Integer id;
+    private String nome;
+    private Double salario;
+    public Funcionario (Integer id, String nome, Double salario){
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public void setId (Integer id){
+        this.id = id;
+    }
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+    public void setSalario (Double salario){
+        this.salario = salario;
+    }
+
+    public Integer getId(){
+        return id;
+    };
+    public String getNome(){return nome;};
+    public Double getSalario(){return salario;};
+
+    public void aumentoSario(Double porcentagem){
+        this.salario += salario * (porcentagem/100);
+    }
+
+
+    public String toString() {
+        return id + ", " + nome + ", " + String.format("%.2f", salario);
+    }
 }
--- exercicios-sessao-10/src/main/java/MatrizExercicio.java
+++ exercicios-sessao-10/src/main/java/MatrizExercicio.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class MatrizExercicio {
+import java.util.Scanner;
+
+public class MatrizExercicio {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de linhas da matriz: ");
+        Integer  linhas = scanner.nextInt();
+
+        System.out.println("Digite a quantidade de colunas da matriz");
+        Integer colunas = scanner.nextInt();
+
+        Integer[][] matriz = new Integer[linhas][colunas];
+
+        for ( int i = 0 ; i < linhas ; i++){
+            for ( int j = 0; j < colunas ; j++){
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Digite um número da matriz: ");
+        Integer numeroMatriz = scanner.nextInt();
+
+        for ( int i = 0 ; i < linhas  ; i++){
+            for ( int j = 0; j < colunas ; j++){
+                if (matriz[i][j] == numeroMatriz) {
+                    System.out.println();
+                    System.out.println("Posição: " + i +", " + j);
+                    if ( j > 0) {
+                        System.out.println("Esquerda: "+ matriz[i][j-1]);
+                    }
+                    if ( i > 0) {
+                        System.out.println("Acima: "+ matriz[i-1][j]);
+                    }
+                    if ( j < matriz[i].length-1) {
+                        System.out.println("direita: "+ matriz[i][j+1]);
+                    }
+                    if ( i < matriz.length-1) {
+                        System.out.println("Abaixo: "+ matriz[i+1][j]);
+                    }
+                }
+            }
+        }
+        scanner.close();
+    }
 }
